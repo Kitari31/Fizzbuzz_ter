a7fc105ec476325d0fdad31890a1d543ed4c436c
Kitari31 <76065071+Kitari31@users.noreply.github.com>
2023-04-20 16:46:11 +0200

    commit 4

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,10 @@ package fizzbuzz;
 public class FizzBuzz {
 
 	public static Object fizzbuzz(int i) {
-		return "" + i;
+		if(i==3)
+			return "fizz";
+		if(i==5)
+			return "buzz";
+		return Integer.toString(i);
 	}
 }
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -16,5 +16,21 @@ class FizzBuzzTest {
 	void fizz_deux() {
 		assertEquals(FizzBuzz.fizzbuzz(2), "2");
 	}
+	
+	@Test
+	void fizz_trois() {
+		assertEquals(FizzBuzz.fizzbuzz(3), "fizz");
+	}
 
+	@Test
+	void fizz_quelques_valeurs() {
+		assertEquals(FizzBuzz.fizzbuzz(4), "4");
+		assertEquals(FizzBuzz.fizzbuzz(7), "7");
+		assertEquals(FizzBuzz.fizzbuzz(400), "400");
+	}
+	
+	@Test
+	void fizz_cinq() {
+		assertEquals(FizzBuzz.fizzbuzz(5), "buzz");
+	}
 }
